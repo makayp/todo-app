89100d8b3c63f7dd97d7eee05b4ecbff1a3732eb
Emmanuel Popoola <popoolae1@gmail.com>
2024-05-01 15:20:01 -0500

    Implement multi-theme feature

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,30 @@
+import { useEffect, useState } from "react";
+import Button from "./Button";
 import User from "./User";
 
+const body = document.body.classList;
 function Header() {
+  const [theme, setTheme] = useState(function () {
+    return localStorage.getItem("Theme") === "pink" ? "pink" : "blue";
+  });
+  useEffect(
+    function () {
+      if (theme === "pink") body.add("pink-theme");
+      localStorage.setItem("Theme", theme);
+    },
+    [theme]
+  );
+
+  function handleChangeTheme() {
+    setTheme(theme => (theme === "pink" ? "blue" : "pink"));
+    body.toggle("pink-theme");
+  }
+
   return (
     <header id='header'>
+      <Button type={`btn-theme ${theme}`} onClick={handleChangeTheme}>
+        <span>Blue</span> <span>Pink</span>
+      </Button>
       <User />
     </header>
   );
